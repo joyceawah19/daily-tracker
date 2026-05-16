2188c0086caa30758f40d28c6c3b69280446ed27
joyce awah <joyce.awah19@gmail.com>
2026-05-16 11:37:44 +0100

    fix auth type error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
+export const dynamic = "force-dynamic";
 "use client";
-
 import { useEffect } from "react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
